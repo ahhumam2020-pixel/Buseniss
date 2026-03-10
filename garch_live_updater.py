712dfc6a85023d1b19a35f7c8be6faff72d0c028
بزنس اونلاین <ahhumam2020@gmail.com>
2026-03-11 07:55:16 +1100

    Update garch_live_updater.py

--- garch_live_updater.py
+++ garch_live_updater.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 """
 GARCH Live Dashboard Updater v3
@@ -74,7 +73,7 @@ MARKETS = {
         "session": "CHINA", "flag": "cn"
     },
     "China A50": {
-        "yahoo":   "^FTXIN9",
+        "yahoo":   "XIN9.F",
         "alpha":   None,
         "tv":      "https://www.tradingview.com/chart/?symbol=ICEEUR:CN1!",
         "session": "CHINA", "flag": "cn"
